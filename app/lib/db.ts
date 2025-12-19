e26864fc2d9eb5c5eb2a024ba64fa4454fa06174
Vishnu <vishnudusa0607@gmail.com>
2025-12-19 19:16:31 +0530

    fix: prisma generate

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-console.log('Initializing Prisma Client');
-const prisma = globalThis.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
-
-export default prisma
+export default prisma;
